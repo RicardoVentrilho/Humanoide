4bb58720a4558e3a023fe619fb3d59656a66c5d9
José Ricardo Ventrilho <ricardoventrilho@gmail.com>
2017-11-15 11:11:12 -0200

    Atividade #4 (Criação da classe Excecao)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include "infraestrutura/utilitarios/excecao.h"
+#include "infraestrutura/cilindro.h"
 
 using namespace std;
+using namespace infraestrutura;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    try
+    {
+        Objeto3D* cilindro = new Cilindro();
+        cilindro->desenhe();
+    }
+    catch(Excecao* erro)
+    {
+        cout << erro->get_mensagem() << endl;
+    }
+
     return 0;
 }
